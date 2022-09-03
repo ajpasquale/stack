9e5d3727abd855547a5f523158632ed73f5b2d46
Andrew Pasquale <kold@Andrews-MacBook-Pro.local>
2022-09-03 17:53:37 -0400

    finished code.

--- stack.go
+++ stack.go
@@ -1,5 +1,7 @@
 package stack
 
+import "errors"
+
 type stack struct {
 	items []int
 }
@@ -10,19 +12,38 @@ func newStack(size int) *stack {
 	}
 }
 
-func (s *stack) Push(item int) {
-	s.items = append(s.items, item)
+func (s *stack) Push(item int) error {
+	if !s.isFull() {
+		s.items = append(s.items, item)
+		return nil
+	}
+	return errors.New("stack is full")
 }
 
-func (s *stack) Pop() {
-	s.items = s.items[:len(s.items)-1]
-
+func (s *stack) Pop() (int, error) {
+	if !s.isEmpty() {
+		item := s.items[len(s.items)-1]
+		s.items = s.items[:len(s.items)-1]
+		return item, nil
+	}
+	return 0, errors.New("stack is empty")
 }
 
-func (s *stack) Peek() int {
-	return s.items[len(s.items)-1]
+func (s *stack) Peek() (int, error) {
+	if !s.isEmpty() {
+		return s.items[len(s.items)-1], nil
+	}
+	return 0, errors.New("stack is empty")
 }
 
 func (s *stack) Size() int {
 	return len(s.items)
+}
+
+func (s *stack) isFull() bool {
+	return len(s.items) == cap(s.items)
+}
+
+func (s *stack) isEmpty() bool {
+	return len(s.items) == 0
 }
--- stack_test.go
+++ stack_test.go
@@ -1,7 +1,6 @@
 package stack
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -15,5 +14,41 @@ func TestNewStack(t *testing.T) {
 func TestStackPeek(t *testing.T) {
 	s := newStack(1)
 	s.Push(10)
-	fmt.Println(s.Peek())
+	if g, _ := s.Peek(); g != 10 {
+		t.Fatal("should have returned ten")
+	}
+	s.Pop()
+	s.Peek()
+}
+
+func TestStackPush(t *testing.T) {
+	s := newStack(1)
+	s.Push(10)
+	s.Push(11)
+	s.Push(1)
+	s.Push(10)
+}
+
+func TestStackPop(t *testing.T) {
+	s := newStack(4)
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	s.Push(12)
+
+	if g, _ := s.Pop(); g != 12 {
+		t.Fatal("should have returned twelve")
+	}
+	if g, _ := s.Pop(); g != 3 {
+		t.Fatal("should have returned three")
+	}
+	if g, _ := s.Pop(); g != 2 {
+		t.Fatal("should have returned two")
+	}
+	if g, _ := s.Pop(); g != 1 {
+		t.Fatal("should have returned one")
+	}
+	if _, err := s.Pop(); err == nil {
+		t.Fatal("should have returned an error")
+	}
 }
