bc096fa05a9e4114e5002dde8dfe61a9f6c5efc3
Andrew Pasquale <kold@Andrews-MacBook-Pro.local>
2022-09-03 18:36:53 -0400

    finished the test cases

--- stack_test.go
+++ stack_test.go
@@ -4,51 +4,38 @@ import (
 	"testing"
 )
 
-func TestNewStack(t *testing.T) {
-	s := newStack(0)
+func TestEntireStack(t *testing.T) {
+	s := newStack(5)
+
+	// testing the stack creation
 	if s == nil {
-		t.Fatal("should return new stack")
+		t.Fatal("should have returned a new stack")
 	}
-}
 
-func TestStackPeek(t *testing.T) {
-	s := newStack(1)
-	s.Push(10)
-	if g, _ := s.Peek(); g != 10 {
-		t.Fatal("should have returned ten")
+	// testing the push
+	for i := 1; i < 6; i++ {
+		if err := s.Push(i); err != nil {
+			t.Fatal("stack should not be full")
+		}
 	}
-	s.Pop()
-	s.Peek()
-}
 
-func TestStackPush(t *testing.T) {
-	s := newStack(1)
-	s.Push(10)
-	s.Push(11)
-	s.Push(1)
-	s.Push(10)
-}
+	if err := s.Push(6); err == nil {
+		t.Fatal("stack should be full")
+	}
 
-func TestStackPop(t *testing.T) {
-	s := newStack(4)
-	s.Push(1)
-	s.Push(2)
-	s.Push(3)
-	s.Push(12)
+	// testing the peek
+	if item, _ := s.Peek(); item != 5 {
+		t.Fatal("top element should be five")
+	}
 
-	if g, _ := s.Pop(); g != 12 {
-		t.Fatal("should have returned twelve")
+	// testing the pop
+	for i := 5; i > 0; i-- {
+		if item, _ := s.Pop(); item != i {
+			t.Fatal("element should be ", i)
+		}
 	}
-	if g, _ := s.Pop(); g != 3 {
-		t.Fatal("should have returned three")
-	}
-	if g, _ := s.Pop(); g != 2 {
-		t.Fatal("should have returned two")
-	}
-	if g, _ := s.Pop(); g != 1 {
-		t.Fatal("should have returned one")
-	}
+
 	if _, err := s.Pop(); err == nil {
-		t.Fatal("should have returned an error")
+		t.Fatal("stack should be empty")
 	}
 }
